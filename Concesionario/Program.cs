391f0aa446e13a778e0310c0e405eb716fbd4cbb
Cristóbal Jara <cristobal.jara@virginiogomez.cl>
2026-09-06 02:08:21 -0300

    api marca

--- Concesionario/Program.cs
+++ Concesionario/Program.cs
@@ -1,49 +1,30 @@
 using Concesionario.Endpoints;
 using Concesionario.Models;
-using Concesionario.Services;
+
 using Scalar.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Scalar.AspNetCore;
 using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-var jwtKey = builder.Configuration["Jwt:Key"];
-var jwtIssuer = builder.Configuration["Jwt:Issuer"];
-var jwtAudince = builder.Configuration["Jwt:Audience"];
 
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = false,
-                ValidateIssuerSigningKey = true,
-                ValidateLifetime = true,
-                ValidIssuer = jwtIssuer,
-                ValidAudience = jwtAudince,
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(jwtKey))
 
-            };
-        });
-builder.Services.AddAuthorization();
+
 
 builder.Services.AddDbContext<ConcesionariodbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("miconexion")));
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddScoped<AuthService>();
+
 
 
 var app = builder.Build();
 
-app.UseAuthentication(); // ¿quién es?
-app.UseAuthorization(); // ¿qué puede hacer?
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -52,3 +33,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapMarcaApi();
+app.Run();
+
